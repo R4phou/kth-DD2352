19e6514b943599ff04fa77891bd3557101acd2c2
Raphaël Humblet <raphum0182@gmail.com>
2024-04-21 19:31:34 +0200

    C:/Program Files/Git/

--- LAB2/graph-to-cast.py
+++ LAB2/graph-to-cast.py
@@ -1,48 +1,84 @@
-def transform_graph_to_casting(V, E, m, edges):
-    # Initialize casting problem input
-    casting_input = {
-        "n": V - 3,
-        "s": E,
-        # + 3 because of the actors 1 and 2 that cannot play in the same scene
-        "k": m + 3,
-        "roles": [],
-        "scenes": [],
-    }
+"""
+- The divas are the first 2 actors
+- The dummy is the last actor
+- The divas cannot play in the same scene    
+The idea is to transform a graph coloring problem input to a casting problem input
+To do this, we split the graph in 2 subgraphs
+    - The first subgraph has 3 nodes: the dummy and the 2 divas
+    - The rest of the graph is a regular coloring problem
+        - Each vertex is a role
+        - Each edge is a scene
+        - Each color is an actor
 
-    # Initialize roles list
-    for i in range(1, V + 1):
-        casting_input["roles"].append([])
+We have 3 colors reserved to color the first subgraph
+And the rest of the colors are used to color the rest of the graph
 
-    # Populate roles
-    for i in range(1, V + 1):
-        casting_input["roles"][i - 1] = [i]  # Role i can be played by actor i
-
-    # Populate scenes
-    for edge in edges:
-        # v1 and v2 are vertices in the graph and thus roles in the casting problem
-        v1, v2 = edge
-        casting_input["scenes"].append([v1, v2])
-
-    return casting_input
+That way, if we can solve the casting problem, we can solve the coloring problem as the transformation just added this dummy graph off to the side
 
 
+Graph coloring problem input:
+V
+E
+m
+e1
+e2
+...
+e_E
+
+Cast problem input:
+n # number of roles
+s # number of scenes
+k # number of actors
+n lines with: length of the line = number of actors that can play that role, and the values are the actors that can play that role
+s lines with: length of the line = number of roles that need to be played in that scene, and the values are the roles that need to be played in that scene
+"""
+
+# Initialize variables
 V = int(input())
 E = int(input())
 m = int(input())
 edges = []
-for i in range(E):
-    line = input().split()
+
+# Read edges
+for line in range(E):
+    line = line.split()
     edges.append((int(line[0]), int(line[1])))
 
-casting_input = transform_graph_to_casting(V, E, m, edges)
+
+# Adding the dummy graph in order to solve the casting problem
+n = V + 3  # Number of roles
+s = E + 3  # Number of scenes
+k = m + 1  # Number of actors
 
 
-# Output:
-# Casting Problem Input:
-print(casting_input["n"])
-print(casting_input["s"])
-print(casting_input["k"])
-for role_actors in casting_input["roles"]:
-    print(len(role_actors), *role_actors)
-for scene_roles in casting_input["scenes"]:
-    print(len(scene_roles), *scene_roles)
+constraint_1 = []  # index = role, value = actors that can play that role
+constraint_2 = []  # index = scene, value = roles that need to be played in that scene
+
+
+all_actors = [i for i in range(k)]
+
+for i in range(3):
+    constraint_1.append([i])  # The divas and the dummy can only play their own role
+
+constraint_2.append([0, 1])  # The first scene needs the divas
+constraint_2.append([0, 2])  # The second scene needs the divas and the dummy
+constraint_2.append([1, 2])  # The third scene needs the divas and the dummy
+
+for i in range(3, n):
+    constraint_1.append(all_actors)  # The rest of the roles can be played by any actor
+
+
+# fill the constraint_2 with the scenes-roles (edges)
+for edge in edges:
+    role1, role2 = edge
+    constraint_2.append([role1, role2])  # Each scene needs the roles in the edge
+
+
+# Print the casting problem
+print(n)
+print(s)
+print(k)
+for actors in constraint_1:
+    print(len(actors), *actors)
+for roles in constraint_2:
+    print(len(roles), *roles)
