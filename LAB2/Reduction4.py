9673d61ced75ede18f1b0653a69e652bc9249e98
Raphaël Humblet <raphum0182@gmail.com>
2024-04-21 23:10:17 +0200

    should be working but non integer output problem

--- LAB2/Reduction4.py
+++ LAB2/Reduction4.py
@@ -2,13 +2,14 @@ def perform_reduction(V, E, m, edges):
     noRoles = V + 4
     noActors = m + 2
     noScenes = E + 2
-    
+
     # Printing the number of roles, scenes, and actors
     print(noRoles)
     print(noScenes)
     print(noActors)
-    
+
     # Divas role constraints
+    # print("Constraint 1:")
     print(1, 1)
     print(1, 2)
 
@@ -16,26 +17,34 @@ def perform_reduction(V, E, m, edges):
     print(1, noActors - 1)
     print(1, noActors)
 
-    # Type 1 Constraints
     for i in range(2, m + 2):
-        print(m, end=' ')
+        temp = []
         for j in range(1, m + 1):
-            if j == m:
-                print(j+2) 
-            else:
-                print(j+2, end=' ')
-        
+            temp.append(j + 2)
+        print(len(temp), *temp)
+
+    # Type 1 Constraints
+    # for i in range(2, m + 2):
+    #     print(m, end=" ")
+    #     for j in range(1, m + 1):
+    #         if j == m:
+    #             print(j + 2)
+    #         else:
+    #             print(j + 2, end=" ")
+
     # Type 2 Constraints
     # For each edge in the input list
+    # print("Constraint 2:")
     for u, v in edges:
-        print(2, u+2, v+2)
-    
+        print(2, u + 2, v + 2)
+
     # Additional constraints for divas and dummies
     print(2, 1, 3)
     print(2, 2, 4)
     # for i in range(1, m + 1):
     #     print(2, E + 1, i)
     #     print(2, E + 2, i)
+
 
 # # Variables
 # V = int(input())
@@ -48,7 +57,7 @@ def perform_reduction(V, E, m, edges):
 #     line = input()
 #     line = line.split()
 #     edges.append((int(line[0]), int(line[1])))
-# # Example usage
+# Example usage
 V = 3
 E = 3
 m = 3
