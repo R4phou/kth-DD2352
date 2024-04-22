ede71e6e5e5f846240c385d25adbe77dc506dab6
Raphaël Humblet <raphum0182@gmail.com>
2024-04-22 11:32:01 +0200

    end lab 2

--- LAB2/Reduction.py
+++ LAB2/Reduction.py
@@ -1,19 +1,30 @@
 def perform_reduction(V, E, m, edges):
+    """
+    Function that perform  the reduction of the graph coloring problem to the casting problem
+    :param V: number of vertices
+    :param E: number of edges
+    :param m: number of colors
+    :param edges: list of edges
+    :return: None
+    """
+    # Isolated vertices
 
-    #Isolated vertices
-
+    scenes = []
     nonAlone = set()
-    for u, v in edges:
+    for u, v in edges:  # E
         nonAlone.add(u)
         nonAlone.add(v)
-    alone = list(set(range(1, V + 1)) - nonAlone)
+        scenes.append((u + 3, v + 3))
+    alone = list(set(range(1, V + 1)) - nonAlone)  # E
 
     # alone = []
     # for i in range(1, V + 1):
     #     if i not in [u for u, v in edges] and i not in [v for u, v in edges]:
     #         alone.append(i)
 
-    noRoles = V + 4
+    m = min(m, V)  # m cannot be greater than V
+
+    noRoles = V + 3
     noScenes = E + 2 + len(alone)
     noActors = m + 2
 
@@ -26,26 +37,25 @@ def perform_reduction(V, E, m, edges):
     print(1, 1)
     print(1, 2)
     print(1, 3)
-    print(1, 3)
 
     # T1 Other role constraints
-    for _ in range(V):
+    for _ in range(V):  # V * m
         temp = []
         for j in range(1, m + 1):
             temp.append(j + 2)
         print(len(temp), *temp)
 
     # T2 standard constraints
-    for u, v in edges:
-        print(2, u + 4, v + 4)
-
+    for u, v in scenes:  # E
+        print(2, u, v)
     # T2 isolated constraints
-    for i in range(len(alone)):
-        print(2, 1, alone[i]+4)
+    for i in range(len(alone)):  # E
+        print(2, 1, alone[i] + 3)
 
     # T2 constraints for divas and dummies
     print(2, 1, 3)
-    print(2, 2, 4)
+    print(2, 2, 3)
+
 
 # Variables
 V = int(input())
--- LAB2/main.py
+++ LAB2/main.py
@@ -12,9 +12,11 @@ k = 3 + m  # 2 divas, a dummy
 scenes = []
 seenRoles = {}
 
+# Constraint 2 for the scene with divas and dummy
 scenes.append((1, 3))
 scenes.append((2, 3))
 
+# Constraint 2 for the rest of the graph
 for _ in range(E):
     u, v = map(int, input().split())
     if u not in seenRoles:
@@ -29,15 +31,18 @@ print(n)
 print(s)
 print(k)
 
+# Constraint 1 for the divas and dummy
 print("1 1")
 print("1 2")
 print("1 3")
 
+# Print constraint 1 for the rest of the roles
 for i in range(4, n + 1):
     print(m, end=" ")
     for j in range(4, k + 1):
         print(j, end=" ")
     print()
 
+# Print constraint 2 for the scenes
 for scene in scenes:
     print("2", *scene)
